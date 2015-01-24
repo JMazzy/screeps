4951e1d5a55d7ad5d7ec0e093ce01c9bd29ef9f4
JMazzy <j.masland@gmail.com>
2015-01-24 16:21:42 -0500

    added comments

--- scripts/guard.js
+++ scripts/guard.js
@@ -1,13 +1,19 @@
 module.exports = function(creep) {
+	//The flag the creep should guard
     var myFlagName = 'g' + creep.memory.number;
     var myFlag = Game.flags[myFlagName];
     
+    //the target the creep should attack if possible
     var target = creep.pos.findClosest( Game.HOSTILE_CREEPS );
     
+    //check if the target exists and is in range
     if ( target && creep.pos.inRangeTo( target, 3 )) {
+        //attack the target
         creep.rangedAttack(target);
     }
+    //check if the flag exists and the creep is not already there
     else if ( myFlag && !creep.pos.inRangeTo( myFlag, 0 ) {
+		//move to the flag
 		creep.moveTo( myFlag );
 	}
 };
